2e7e3833df2a032e22a5cf5ed4af77cdc1dca663
矢野目佳太 <yanomekeifutoshi@yanomekeifutoshinoMacBook-puro.local>
2018-11-15 18:17:47 +0900

    upload

---
+++ AVPro_test/demo_system/Assets/MyMediaPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using RenderHeads.Media.AVProVideo;
+
+// Media Player(Script)に、このクラスをAdd Componentしてください。
+//
+//
+//
+public class MyMediaPlayer : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp("a"))
+        {
+            //シーク移動
+            MediaPlayer m_MediaPlayer = this.gameObject.GetComponent<MediaPlayer>();//自分のコンポーネントから
+            IMediaControl control = m_MediaPlayer.Control;
+            control.Pause();//単位は、ミリセカンド(1000分の１秒)
+
+            Debug.Log("A Key UP");
+
+        }
+
+      
+    }
+}
+
